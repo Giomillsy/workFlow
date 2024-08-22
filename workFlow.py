30ec538960315791feb4c7e2dbb2c0391cb71499
Owen Mills <owen.millst@tpt.org.uk>
2024-08-22 23:56:25 +0100

    Created first pre-set task

--- workFlow.py
+++ workFlow.py
@@ -1,4 +1,23 @@
+class Task:
+    def __init__(self):
+        self.taskType = "FMA Update"
+        self.steps = loadTaskSteps(self.taskType)
 
+class Step:
+    def __init__(self,stepName,nextSteps = [], end=False):
+        self.stepName = stepName
+        self.nextSteps = nextSteps
+        self.end = end
+
+
+def loadTaskSteps(taskType):
+    return [Step("Update FMA Schedule",[1]),
+            Step("Create ammendment letter",[1]),
+            Step("Review FMA",[-2,1]),
+            Step("Review Ammendment Letter",[-2,1]),
+            Step("Send FMA documents to Leeds",[1]),
+            Step("Save documents",end=True)
+            ]
 
 def main():
     #main function
@@ -12,6 +31,7 @@ def main():
             #Write code here for saving the existing objects
             print("Closing the code")
             exit()
+        
 
 def menu():
     print("----------------")
@@ -20,6 +40,8 @@ def menu():
     print("3: Exit the code")
     print("----------------")
 
+    
+    #Error catching
     try:
         choice = int(input("Please enter an option:"))
     except ValueError:
