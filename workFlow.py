12fd43680cfd2bd6d9763164cc4b40fa6b7a6994
Owen Mills <owen.millst@tpt.org.uk>
2024-08-23 11:59:12 +0100

    first test

--- workFlow.py
+++ workFlow.py
@@ -1,13 +1,31 @@
+#Imports
+import pickle
+
+
 class Task:
-    def __init__(self):
-        self.taskType = "FMA Update"
+    def __init__(self,taskType):
+        self.taskType = taskType
         self.steps = loadTaskSteps(self.taskType)
+        self.spos = 0
+        self.getNextSteps()
+
+    def getNextSteps(self):
+        #Gets the next possible steps in the work flow
+    
+        self.nextSteps = []
+        for rel in self.steps[self.spos].nextStepsRel:
+            self.nextSteps.append(self.steps[self.pos+rel])
+
+    def navigateWf(self):
+        print("The current work flow step is:",self.steps[self.spos].stepName)    
+
 
 class Step:
-    def __init__(self,stepName,nextSteps = [], end=False):
+    def __init__(self,stepName,nextStepsRel = []):
         self.stepName = stepName
-        self.nextSteps = nextSteps
-        self.end = end
+        self.nextStepsRel = nextStepsRel
+
+
 
 
 def loadTaskSteps(taskType):
@@ -16,8 +34,24 @@ def loadTaskSteps(taskType):
             Step("Review FMA",[-2,1]),
             Step("Review Ammendment Letter",[-2,1]),
             Step("Send FMA documents to Leeds",[1]),
-            Step("Save documents",end=True)
+            Step("Save documents")
             ]
+
+
+def startNewTask():
+    print("-----------")
+    print("Select one of the below to create it as a task")
+    print("1: FMA Update")
+    print("2: Exit back to main menu")
+    print("--------------")
+
+    choice = getChoice(2)
+    if choice ==1:
+        newTask = Task("FMA Update")
+        with open("taskStorage.txt","w") as f:
+            pickle.dump(newTask,f)
+        print("Created task")
+
 
 def main():
     #main function
@@ -31,7 +65,21 @@ def main():
             #Write code here for saving the existing objects
             print("Closing the code")
             exit()
-        
+        elif choice == 1:
+            startNewTask()
+
+def getChoice(maxChoice):
+    
+    #Error catching
+    try:
+        choice = int(input("Please enter an option:"))
+    except ValueError:
+        print("You did not enter a number. Can you try again please")
+
+    if choice not in range(1,maxChoice+1):
+        print("You did not enter a valid number. Can you try again please")
+
+    return choice      
 
 def menu():
     print("----------------")
@@ -40,17 +88,7 @@ def menu():
     print("3: Exit the code")
     print("----------------")
 
-    
-    #Error catching
-    try:
-        choice = int(input("Please enter an option:"))
-    except ValueError:
-        print("You did not enter a number. Can you try again please")
-
-    if choice < 1 or choice >3:
-        print("You did not enter a valid number. Can you try again please")
-
-    return choice
+    return getChoice(3)
         
 
 if __name__ == "__main__":
